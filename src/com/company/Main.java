64cba764e46933695ae1955b0229828802f58707
Paguro <ivankosaev2006@gmail.com>
2023-06-05 12:21:50 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,8 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Singleton calculator = Singleton.getInstance();
         System.out.println(calculator.calculator(30, 60));
     }
